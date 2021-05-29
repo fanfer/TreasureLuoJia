5ae42d802709033ab44d13e3334ed5bcf6ac9ce7
sun-yue838 <3272168006@qq.com>
2021-05-29 14:31:50 +0800

    sy all npc

--- Assets/Scripts/sy/npc02.cs
+++ Assets/Scripts/sy/npc02.cs
@@ -6,10 +6,12 @@ public class npc02 : MonoBehaviour
 {
     public bool can_talk = false;
     public int count = 0;
+    public GUIStyle style;
     // Start is called before the first frame update
     void Start()
     {
-        
+        style.fontSize = 20;
+        style.normal.textColor = new Color(255, 255, 255);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -42,11 +44,11 @@ public class npc02 : MonoBehaviour
         {
 
             if (count == 1)
-                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "你好，whuer");
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "你好，whuer",style);
             if (count == 2)
-                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "听说，来武大看一次人海，是每朵樱花的梦想");
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "听说，来武大看一次人海，是每朵樱花的梦想", style);
             if (count == 3)
-                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "给whuer看一场烟花，是我的梦想");
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "给whuer看一场烟花，是我的梦想", style);
             if (count == 4)
             {
                 //烟花
@@ -54,9 +56,9 @@ public class npc02 : MonoBehaviour
                 //捡碎片
             }
             if (count == 5)
-                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "恭喜whuer又收获新的碎片！");
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "恭喜whuer又收获新的碎片！", style);
             if (count == 6)
-                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "哦对了，不要放过遇见过的魔法阵哦，去找找新发现吧");
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "哦对了，不要放过遇见过的魔法阵哦，去找找新发现吧", style);
         }
 
     }
---
+++ Assets/Scripts/sy/npc03.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc03 : MonoBehaviour
+{
+    public bool can_talk;
+    public int count = 0;
+    public GUIStyle style;
+    // Start is called before the first frame update
+    void Start()
+    {
+        style.fontSize = 20;
+        style.normal.textColor = new Color(255, 255, 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (can_talk == true)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                count++;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "unitychan")
+        {
+            can_talk = true;
+            Debug.Log("OnCollisionEnter碰撞触发");
+        }
+    }
+
+
+
+    void OnGUI()
+
+    {
+
+        if (can_talk)
+
+        {
+            
+
+            if (count == 1)
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "你好，whuer", style);
+            if (count == 2)
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "我是珞珈虚拟世界的珈珈", style);
+            if (count == 3)
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "珞珞如玉，珈珈如石，是为珞珈", style);
+            if (count == 4)
+            {
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "不管何时，都欢迎回珈", style);
+            }
+            /*
+            if (count == 5)
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "恭喜whuer又收获新的碎片！");
+            if (count == 6)
+                GUI.Label(new Rect(Screen.width * 0.52f, (float)(Screen.height * 0.5), 300, 100), "哦对了，不要放过遇见过的魔法阵哦，去找找新发现吧");
+            */
+        }
+
+    }
+}
+
+
