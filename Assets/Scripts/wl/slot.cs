aab46df8de7fe1c440eacc3cb34b23a96c173c98
乐 王 <2019302110153@whu.edu.cn>
2021-06-06 23:04:23 +0800

    背包3.0

---
+++ Assets/Scripts/wl/slot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slot : MonoBehaviour
+{
+    public Items slotItem;
+    public Image slotImage;
+    public Text slotNum;
+}
